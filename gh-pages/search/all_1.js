b41ccbe9712aed07096b1cfdb87c0ee62be39ade
Robert Wolff <bob.wolff68@gmail.com>
2023-04-17 11:54:08 -0700

    doc-update

--- gh-pages/search/all_1.js
+++ gh-pages/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['test_20of_20the_20main_20page_2e_20try_204_20from_20main_0',['Test of the main page. try 4 from main',['../index.html',1,'']]]
+  ['test_20of_20the_20main_20page_2e_20try_205_20from_20main_0',['Test of the main page. try 5 from main',['../index.html',1,'']]]
 ];
