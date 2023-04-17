12d5f3b69a5ba159d385ba2cd3e7768ae904df2e
Robert Wolff <bob.wolff68@gmail.com>
2023-04-17 10:05:36 -0700

    Only real checkin

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+/** @file main.cpp
+ * @brief Main program for test
+*/
+
+
+/**
+ * \mainpage Test of the main page.
+ * 
+ * Here we are hoping gh-pages works.
+ * 
+ */
+
+/**
+ * @brief Main function entry point.
+ *
+ */
+int main() {
+	printf("Hello world.\n");
+}
